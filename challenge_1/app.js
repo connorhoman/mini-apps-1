8833a42716e195b9365d785e93b792ee7866b40e
Connor Homan <connorhoman14@gmail.com>
2019-05-06 11:27:17 -0700

    Created board, implemented click/turn functionality

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,19 @@
+// Model --------------------------------
+
+var currentTurn = 'X';
 
 
+
+
+// Controllers --------------------------
+
+var changeSquare = function(square) {
+	if (!square.innerHTML) {
+		square.innerHTML = currentTurn;
+		if (currentTurn === 'X') {
+			currentTurn = 'O'; 
+		} else if (currentTurn === 'O') {
+			currentTurn = 'X';
+		}
+	}
+}
