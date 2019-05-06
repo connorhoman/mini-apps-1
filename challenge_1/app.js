8e76ebba07f0584e748aa068ee74aacbf9621002
Connor Homan <connorhoman14@gmail.com>
2019-05-06 14:28:31 -0700

    BMR functional

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,41 +3,6 @@
 var currentTurn = 'X';
 var gameWon = false;
 
-var winChecker = function() {
-	var board = document.getElementsByClassName('square');
-	var pieces = [];
-	var winningLayouts = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-	for (var i = 0; i < board.length; i++) {
-		pieces.push(board[i].innerHTML);
-	}
-
-	for (var i = 0; i < winningLayouts.length; i++) {
-		var layout = winningLayouts[i];
-		if (pieces[layout[0]] === pieces[layout[1]] && pieces[layout[0]] === pieces[layout[2]]) {
-			if (pieces[layout[0]] !== '') {
-				displayWin();
-				debugger;
-				gameWon = true;
-				currentTurn = pieces[layout[0]];
-				break;
-			}
-		}
-	}
-
-	if (!gameWon) {
-		var tied = true;
-		for (var i = 0; i < pieces.length; i++) {
-			if (!pieces[i]) {
-				tied = false;
-			}
-		}
-		if (tied) {
-			displayTie();
-		};
-	}
-}
-
-
 // Controllers --------------------------
 
 var changeSquare = function(square) {
@@ -66,6 +31,39 @@ var newGame = function() {
 
 
 // View --------------------------
+
+var winChecker = function() {
+	var board = document.getElementsByClassName('square');
+	var pieces = [];
+	var winningLayouts = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+	for (var i = 0; i < board.length; i++) {
+		pieces.push(board[i].innerHTML);
+	}
+
+	for (var i = 0; i < winningLayouts.length; i++) {
+		var layout = winningLayouts[i];
+		if (pieces[layout[0]] === pieces[layout[1]] && pieces[layout[0]] === pieces[layout[2]]) {
+			if (pieces[layout[0]] !== '') {
+				displayWin();
+				gameWon = true;
+				currentTurn = pieces[layout[0]];
+				break;
+			}
+		}
+	}
+
+	if (!gameWon) {
+		var tied = true;
+		for (var i = 0; i < pieces.length; i++) {
+			if (!pieces[i]) {
+				tied = false;
+			}
+		}
+		if (tied) {
+			displayTie();
+		};
+	}
+}
 
 var displayWin = function() {
 	window.alert(currentTurn + ' has WON!');
