ebed6a2d6c78fd120b1bcc773b990cdf283271b1
Connor Homan <connorhoman14@gmail.com>
2019-05-06 11:49:45 -0700

    Set up rest of the functions needed for Basic Reqs, now beginning Tic-Tac-Toe rules

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,7 +2,22 @@
 
 var currentTurn = 'X';
 
+var changeTurn = function() {
+	if (currentTurn === 'X') {
+			currentTurn = 'O'; 
+		} else if (currentTurn === 'O') {
+			currentTurn = 'X';
+		}
+}
 
+var winChecker = function() {
+	var board = document.getElementsByClassName('square');
+	var pieces = [];
+	for (var i = 0; i < board.length; i++) {
+		pieces.push(board[i].innerHTML);
+	}
+	// Implement tic-tac-toe rules
+}
 
 
 // Controllers --------------------------
@@ -10,10 +25,22 @@ var currentTurn = 'X';
 var changeSquare = function(square) {
 	if (!square.innerHTML) {
 		square.innerHTML = currentTurn;
-		if (currentTurn === 'X') {
-			currentTurn = 'O'; 
-		} else if (currentTurn === 'O') {
-			currentTurn = 'X';
-		}
+		winChecker();
+		changeTurn();
 	}
+}
+
+
+// View --------------------------
+
+var newGame = function() {
+	// wipe board clean
+}
+
+var displayWin = function() {
+	// Show "WINNER" and X or O
+}
+
+var displayTie = function() {
+	// Show "TIE"
 }
