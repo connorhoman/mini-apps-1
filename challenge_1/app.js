bb3efb8fb4399d8a9f7cb70a5aa7ac7698be0283
Connor Homan <connorhoman14@gmail.com>
2019-05-06 12:22:43 -0700

    Began implementing game rules (win isnt working, tie is)

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -12,11 +12,33 @@ var changeTurn = function() {
 
 var winChecker = function() {
 	var board = document.getElementsByClassName('square');
+	var gameWon;
 	var pieces = [];
+	var winningLayouts = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 	for (var i = 0; i < board.length; i++) {
 		pieces.push(board[i].innerHTML);
 	}
-	// Implement tic-tac-toe rules
+
+
+	for (var i = 0; i < winningLayouts.length; i++) {
+		var layout = winningLayouts[i];
+		if (pieces[layout[0]] === pieces[layout[1]] && pieces[layout[0]] === pieces[layout[2]]) {
+			displayWin();
+			gameWon = true;
+			break;
+		}
+	}
+	if (!gameWon) {
+		var tied = true;
+		for (var i = 0; i < pieces.length; i++) {
+			if (!pieces[i]) {
+				tied = false;
+			}
+		}
+		if (tied) {
+			displayTie();
+		};
+	}
 }
 
 
@@ -30,17 +52,17 @@ var changeSquare = function(square) {
 	}
 }
 
-
-// View --------------------------
-
 var newGame = function() {
 	// wipe board clean
 }
 
+
+// View --------------------------
+
 var displayWin = function() {
-	// Show "WINNER" and X or O
+	console.log(currentTurn, 'has WON!');
 }
 
 var displayTie = function() {
-	// Show "TIE"
+	console.log('TIE!')
 }
