d4cf92fd21c56d2bbb13f6376efedea1637d6a02
Connor Homan <connorhoman14@gmail.com>
2019-05-06 14:03:15 -0700

    Bare Minimum Requirements complete

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,18 +1,10 @@
 // Model --------------------------------
 
 var currentTurn = 'X';
-
-var changeTurn = function() {
-	if (currentTurn === 'X') {
-			currentTurn = 'O'; 
-		} else if (currentTurn === 'O') {
-			currentTurn = 'X';
-		}
-}
+var gameWon = false;
 
 var winChecker = function() {
 	var board = document.getElementsByClassName('square');
-	var gameWon;
 	var pieces = [];
 	var winningLayouts = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 	for (var i = 0; i < board.length; i++) {
@@ -24,7 +16,9 @@ var winChecker = function() {
 		if (pieces[layout[0]] === pieces[layout[1]] && pieces[layout[0]] === pieces[layout[2]]) {
 			if (pieces[layout[0]] !== '') {
 				displayWin();
+				debugger;
 				gameWon = true;
+				currentTurn = pieces[layout[0]];
 				break;
 			}
 		}
@@ -47,11 +41,19 @@ var winChecker = function() {
 // Controllers --------------------------
 
 var changeSquare = function(square) {
-	if (!square.innerHTML) {
+	if (!square.innerHTML && !gameWon) {
 		square.innerHTML = currentTurn;
 		winChecker();
 		changeTurn();
 	}
+}
+
+var changeTurn = function() {
+	if (currentTurn === 'X' && !gameWon) {
+			currentTurn = 'O'; 
+		} else if (currentTurn === 'O' && !gameWon) {
+			currentTurn = 'X';
+		}
 }
 
 var newGame = function() {
@@ -59,16 +61,16 @@ var newGame = function() {
 	for (var i = 0; i < board.length; i++) {
 		board[i].innerHTML = '';
 	}
-	currentTurn = 'X';
+	gameWon = false;
 }
 
 
 // View --------------------------
 
 var displayWin = function() {
-	console.log(currentTurn, 'has WON!');
+	window.alert(currentTurn + ' has WON!');
 }
 
 var displayTie = function() {
-	console.log('TIE!')
+	window.alert('TIE!')
 }
