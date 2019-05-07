d188d289b8030f93a052dbd530b8fb3bd9b08f85
Connor Homan <connorhoman14@gmail.com>
2019-05-07 16:03:45 -0700

    Server functional, data is passing through to empty parser function

---
+++ challenge_2/server/parseCSV.js
@@ -0,0 +1,5 @@
+var exports = module.exports; 
+
+exports.parseCSV = function(JSON) {
+	console.log('trying to parse', JSON);
+}
--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const parse = require('./parseCSV.js');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
+
+app.post('/', function(req, res) {
+	console.log('Recieved POST Request');
+	res.end(parse.parseCSV(req.body.textInput));
+});
 
 app.listen(port, () => console.log('Listening on', port));
-app.use(express.static('public'));
 
 
 
