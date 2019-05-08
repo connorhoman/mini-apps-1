6fb2127517b1a1d8c32f2d3d0e0f19f10a119d40
Connor Homan <connorhoman14@gmail.com>
2019-05-08 12:23:11 -0700

    Database connected

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,10 +1,33 @@
+// SERVER and DATABASE connections ----------------------
+
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'connorhoman',
+});
+
+db.connect(function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+	console.log("Connected to MySQL Database");
+  }
+})
 
 app.use(express.static('public'));
-
 app.listen(port, () => console.log('Listening on port: ', port)); 
+
+
+
+// ROUTES -----------------------------------------------
+
+
+
+
 
 
 
