fd51765bd82c131f096aaa7fbb7dbd48e359473f
Connor Homan <connorhoman14@gmail.com>
2019-05-07 19:04:32 -0700

    Parsing functionality nearly complete.  Renders new form along with parsed data

--- challenge_2/server/parseCSV.js
+++ challenge_2/server/parseCSV.js
@@ -1,8 +1,29 @@
 var exports = module.exports; 
 
 exports.parseCSV = function(data) {
+  var obj = JSON.parse(data);
+  var result = [];
 
-	console.log(JSON.parse(data).firstName);
+  var keys = [];
+  for (var key in obj) {
+	if (key !== 'children') {
+		keys.push(key);
+	}
+  }
+	
+  var recursive = function(node) {
+    for (var i = 0; i < keys.length; i++) {
+	  result.push(node[keys[i]]);
+	}
+	if (node.children.length > 0) {
+	  for (var j = 0; j < node.children.length; j++) {
+	    recursive(node.children[j]);
+	  }
+	}
+  }
+
+  recursive(obj);
+  return result.join(',');
 }
 
 // The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single 
--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -9,6 +9,20 @@ app.use(bodyParser.urlencoded({extended: true }));
 
 app.post('/', function(req, res) {
   console.log('Recieved POST Request');
+  res.write(`<!DOCTYPE html>
+	<html> 
+	  <head>
+	  </head>
+
+	  <body>
+	  	<h1> CSV Report Generator: </h1>
+	  	<form action='http://127.0.0.1:3000/' method='POST'> 
+	  		<textarea id='textInput' type='text' name='textInput' rows='8' cols='100'></textarea>
+	  		<input id='submit' type='submit' value='Submit JSON'/>
+	  	</form>
+	  </body>
+	  <script src='app.js'></script>
+	</html>`)
   res.end(parse.parseCSV(req.body.textInput));
 });
 
