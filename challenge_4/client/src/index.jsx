b4478f4291728ccbf2fb1623c27b4abc095fb9c5
Connor Homan <connorhoman14@gmail.com>
2019-05-09 12:06:37 -0700

    Started React development, fixed webpack presets

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -1,2 +1,10 @@
-console.log('hello!!!');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
+class App extends React.Component {
+	render() {
+		return ();
+	}
+}
+ 
+ReactDOM.render(<App />, document.getElementById('app'));
