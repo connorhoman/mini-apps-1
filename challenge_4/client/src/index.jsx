4652bdf4ecb4bf197f9697103cbe4cbe17d383a3
Connor Homan <connorhoman14@gmail.com>
2019-05-09 14:43:16 -0700

    Created board

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -2,27 +2,25 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 class Board extends React.Component {
-
-	createBoard() { 
-		let board = [];
-		
-	    for (let i = 0; i < 7; i++) {
-	      let children = []
-	      for (let j = 0; j < 8; j++) {
-	        children.push(<td>{`Column ${j + 1}`}</td>)
-	      }
-	      board.push(<tr>{children}</tr>)
-	    }
+  createBoard() { 
+	let board = [];
+	for (let i = 0; i < 7; i++) {
+	  let children = [];
+	  for (let j = 0; j < 8; j++) {
+        children.push(<td>{`${i + 1} ${j + 1}`}</td>)
+      }
+      board.push(<tr>{children}</tr>);
+	}
     return board;
-	}
+  }
 
-	render() {
-		return (
-			<table key='board'>
-				{this.createBoard()}
-			</table>
-		);
-	}
+  render() {
+	return (
+   	  <table key='board'>
+   		{this.createBoard()}
+	  </table>
+	);
+  }
 }
 
 ReactDOM.render(<Board />, document.getElementById('board'));
