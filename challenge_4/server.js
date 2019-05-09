badf88b30f75ef95ef84afce7bcba5529c5f57d5
Connor Homan <connorhoman14@gmail.com>
2019-05-09 11:41:30 -0700

    Created environment and HTML skeleton, server is connected

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('client/dist/'));
 
+app.get('/', (req, res) => {
+	console.log('Received GET request!');
+});
+
+app.listen(port, (err) => {
+	if (err) {
+		console.log('ERROR', err) 
+	} else {
+		console.log('Listening to Port:', port);
+	}
+});
