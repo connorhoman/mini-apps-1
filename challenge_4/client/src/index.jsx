11c3115ced9b02dacc7caf695faaa14f20d4f051
Connor Homan <connorhoman14@gmail.com>
2019-05-09 16:11:24 -0700

    Working on board functionality

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Board extends React.Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {turn: 'player1'};
+  	this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+  	console.log('board clicked');
+  }
+
   createBoard() { 
 	let board = [];
 	for (let i = 0; i < 7; i++) {
 	  let children = [];
 	  for (let j = 0; j < 8; j++) {
-        children.push(<td key={`${i}, ${j}`}></td>)
+        children.push(<Square handleClick={this.handleClick} id={`${i}, ${j}`} key={`${i}, ${j}`} />)
       }
       board.push(<tr key={`${i}`}>{children}</tr>);
 	}
@@ -26,26 +36,28 @@ class Board extends React.Component {
 }
 
 class Square extends Board {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+	super(props);
+	this.state = {
+	  player1: false,
+	  player2: false,
+	  playable: true
 	}
+  }
 
-	render() {
+  handleClick() {
+ 	this.props.handleClick();
+  }
 
-	}
+  render() {
+  	if (this.state.player1) {
+  	  return (<td style="background-color: blue" onClick={() => {this.handleClick()}} ></td>)
+  	}
+  	if (this.state.player2) {
+  	  return (<td style="background-color: red" onClick={() => {this.handleClick()}} ></td>)
+  	}
+	return (<td onClick={() => {this.handleClick()}} ></td>);
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 ReactDOM.render(<Board />, document.getElementById('app'));
-
-export default Board;
