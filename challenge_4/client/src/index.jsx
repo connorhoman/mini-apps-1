fe01ff8ec0dc3ad0e1f246da5aca20f7c9912035
Connor Homan <connorhoman14@gmail.com>
2019-05-09 17:19:18 -0700

    Can access state of the board, but only as of before the previous turn

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -5,21 +5,41 @@ class Board extends React.Component {
   constructor(props) {
   	super(props);
   	this.state = {turn: 'player1'};
-  	this.handleClick = this.handleClick.bind(this)
+  	this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
   	if (this.state.turn === 'player1') {
-  		this.setState({turn: 'player2'})
+  		this.setState({turn: 'player2'});
   	}
   	if (this.state.turn === 'player2') {
-  		this.setState({turn: 'player1'})
+  		this.setState({turn: 'player1'});
   	}
   	this.winChecker();
   }
 
   winChecker() {
-  	
+  	var getSquare = function(coords) {
+  	  var ele = document.getElementById(coords);
+  		
+  	  if (ele.getAttribute('player1') === 'true') {
+  		return 1;
+  	  } else if (ele.getAttribute('player2') === 'true') {
+  		return 2;
+  	  } else {
+  		return 0;
+  	  } 
+  	}
+
+  	var result = [];
+  	for (var i = 0; i < 6; i++) {
+  	  for (var j = 0; j < 7; j++) {
+  		var set = i.toString() + ', ' + j.toString();
+  		result.push(getSquare(set));
+  	  }
+  	}
+
+  	console.log(result);
   }
 
   createBoard() { 
@@ -27,7 +47,7 @@ class Board extends React.Component {
 	for (let i = 0; i < 7; i++) {
 	  let children = [];
 	  for (let j = 0; j < 8; j++) {
-        children.push(<Square handleClick={this.handleClick} turn={this.state.turn} id={`${i}, ${j}`} key={`${i}, ${j}`} />)
+        children.push(<Square handleClick={this.handleClick} turn={this.state.turn} id={`${i}, ${j}`} key={`${i}, ${j}`} />);
       }
       board.push(<tr key={`${i}`}>{children}</tr>);
 	}
@@ -70,12 +90,12 @@ class Square extends Board {
   	let styles2 = {backgroundColor: 'blue'}
 
   	if (this.state.player1) {
-  	  return (<td style={styles1} onClick={() => {this.handleClick()}} ></td>);
+  	  return (<td style={styles1} player1={this.state.player1.toString()} id={this.props.id} onClick={() => {this.handleClick()}} ></td>);
   	}
   	if (this.state.player2) {
-  	  return (<td style={styles2} onClick={() => {this.handleClick()}} ></td>);
+  	  return (<td style={styles2} player2={this.state.player2.toString()} id={this.props.id} onClick={() => {this.handleClick()}} ></td>);
   	}
-	return (<td onClick={() => {this.handleClick()}} ></td>);
+	return (<td id={this.props.id} onClick={() => {this.handleClick()}} ></td>);
   }
 }
 
